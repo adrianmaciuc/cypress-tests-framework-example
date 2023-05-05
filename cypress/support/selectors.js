191ea5951c2b3e716e48804c7e91d804a595c949
Adrian Maciuc <adrian.maciuc@gmail.com>
2023-05-06 02:43:01 +0300

    first selectors from home page

--- cypress/support/selectors.js
+++ cypress/support/selectors.js
@@ -1,4 +1,21 @@
+
 export const SELECTORS = {
-	usernameField : '[id="user-name"]',
-	passwordField: '[data-test="password"]'
+	HOME_PAGE: {
+		NAVBAR: {
+			whatsNew : '[id="ui-id-3"]',
+			women: '[id="ui-id-4"]',
+			men: '[id="ui-id-5"]',
+			gear: '[id="ui-id-6"]',
+			training: '[id="ui-id-7"]',
+			sale: '[id="ui-id-8"]',
+		},
+		productItem : '.product-item',
+		blocksPromoLinks : '.blocks-promo a'
+	},
+	OTHER : {
+	},
+	HTML : {
+		a: 'a',
+
+	}
 }
