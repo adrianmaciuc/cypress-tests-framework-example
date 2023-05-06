45aaafddb19d7a1324e451ac6731f12a8965bb70
Adrian Maciuc <adrian.maciuc@gmail.com>
2023-05-06 23:51:14 +0300

    intentional fail

--- cypress/specs/home-page.spec.js
+++ cypress/specs/home-page.spec.js
@@ -5,7 +5,7 @@ const HTML = SELECTORS.HTML
 describe('Landing page', () => {
 	it('Verify basic components of home page are loaded and links are crawlable', () => {
 		cy.visit('/')
-		cy.get(HOME_PAGE.blocksPromoLinks).should('have.length', 6)
+		cy.get(HOME_PAGE.blocksPromoLinks).should('have.length', 7)
 		// cy.get(HOME_PAGE.blocksPromoLinks).each(function(item){
 		// 	cy.request(item[0].href).its('status').should('eq', 200)
 		// 	cy.wait(3500)
