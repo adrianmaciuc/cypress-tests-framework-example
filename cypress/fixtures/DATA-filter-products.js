a805b60e98adc8b008638683fa6017b6551ae249
Adrian Maciuc <adrian.maciuc@gmail.com>
2023-05-10 00:51:07 +0300

    new test filter products

---
+++ cypress/fixtures/DATA-filter-products.js
@@ -0,0 +1,11 @@
+const filterProduct = () => {
+	const data = {
+		category1: 'Style',
+		category2: 'Price',
+		styleOption: 'Hoodie',
+		priceOption: '$60.00 and above',
+	}
+	return data
+}
+
+module.exports = filterProduct 
---
+++ cypress/specs/Filtering-products.spec.js
@@ -0,0 +1,36 @@
+import { SELECTORS } from '../support/selectors'
+import { ROUTES } from '../support/routes'
+const filterProducts = require('../fixtures/DATA-filter-products')
+
+const { PRODUCT_FILTERS, PRODUCT } = SELECTORS
+
+// test data
+let data = filterProducts()
+// end of test data
+
+describe('Filtering functionalities', () => {
+	it('Filter by style and price', () => {
+		cy.visit('/' + ROUTES.womenTopsCategories.hoodies)
+		cy.get(PRODUCT.productItem).should('have.length', 12)
+		cy.get(PRODUCT_FILTERS.filterItem).contains(data.category1).click()
+		cy.get(PRODUCT_FILTERS.filterItemActive).find(PRODUCT_FILTERS.item).contains(data.styleOption).should('be.visible').click()
+		cy.get(PRODUCT.productItem).should('have.length', 9)
+
+		cy.get(PRODUCT_FILTERS.filterItem).contains(data.category2).click()
+		cy.get(PRODUCT_FILTERS.filterItemActive).find(PRODUCT_FILTERS.item).contains(data.priceOption).should('be.visible').click()
+		cy.get(PRODUCT.productItem).should('have.length', 1)
+	})
+	it('Sorting products by Price', () => {
+		cy.visit('/' + ROUTES.womenTopsCategories.brasNtanks)
+		cy.get(PRODUCT.priceContainer).first().then((element)=>{
+			// clean data and extract only price as integer
+			data.valueBeforeSort = Number(element.text().split('$')[1].split('.')[0])
+		})
+
+		cy.get(PRODUCT.sorter).first().select('price')
+		cy.get(PRODUCT.priceContainer).first().then((element)=>{
+			data.valueAfterSort = Number(element.text().split('$')[1].split('.')[0])
+			expect(data.valueBeforeSort).to.be.greaterThan(data.valueAfterSort)
+		})
+	})
+})
