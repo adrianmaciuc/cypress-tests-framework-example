417c285ffa43686b56e92eb36dc119a4ef3a850d
Adrian Maciuc <adrian.maciuc@gmail.com>
2023-05-10 00:49:44 +0300

    add more routes

--- cypress/support/routes.js
+++ cypress/support/routes.js
@@ -4,5 +4,13 @@ export const ROUTES = {
 	men: 'men.html',
 	gear: 'gear.html',
 	training: 'training.html',
-	sale: 'sale.html'
+	sale: 'sale.html',
+	womenTops : 'women/tops-women.html',
+	womenBottoms : 'women/bottoms-women.html',
+	womenTopsCategories : {
+		jackets : 'women/tops-women/jackets-women.html',
+		hoodies : 'women/tops-women/hoodies-and-sweatshirts-women.html',
+		tees : 'women/tops-women/tees-women.html',
+		brasNtanks : 'women/tops-women/tanks-women.html',
+	}
 }
