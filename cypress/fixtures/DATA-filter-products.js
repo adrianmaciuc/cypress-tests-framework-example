7cb0dfd87cfba7c75b0d6d3d6c5dccf534d69652
Adrian Maciuc <adrian.maciuc@gmail.com>
2023-05-11 23:49:02 +0300

    export fix

--- cypress/fixtures/DATA-filter-products.js
+++ cypress/fixtures/DATA-filter-products.js
@@ -1,4 +1,4 @@
-export const filterProduct = () => {
+const filterProduct = () => {
 	const data = {
 		category1: 'Style',
 		category2: 'Price',
@@ -7,3 +7,5 @@ export const filterProduct = () => {
 	}
 	return data
 }
+
+module.exports = filterProduct
