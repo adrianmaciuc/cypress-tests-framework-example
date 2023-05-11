3de07c43facd3afcf2995adc92d6b7f1549011cb
Adrian Maciuc <adrian.maciuc@gmail.com>
2023-05-11 23:35:53 +0300

    update comment

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -3,7 +3,8 @@ import './commands'
 
 Cypress.on('uncaught:exception', (err, runnable) => {
 	// returning false here prevents Cypress from
-	// failing the test
+	// failing the test in case the web-app has errors
+	// The app not our tests
 	return false
 })
 
